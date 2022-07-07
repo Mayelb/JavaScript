f2bd32f03f5014679c67f0dcebafa82c9abcb814
mayelb <mayelb22@gmail.com>
2022-07-06 23:25:43 -0300

    Pre-entrega proyecto

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,11 +1,13 @@
-alert("Bienvenido al BAZAR ROSARITO");
-
-class Compra {
-    constructor ( producto, precio, cantidad) {
+alert("Bienvenido al BAZAR ROSARITO"); 
+  
+class Items {
+    constructor ( producto, precio,) {
         this.producto = producto;
         this.precio = precio;
     }
 }
+
+ let cliente;
 
  let stock = [
     {producto:"taza", precio:"500"},
@@ -15,38 +17,58 @@ class Compra {
     {producto:"pantumedias", precio:"900"},     
  ];
 
-function iniciodecompra(arrayItems) {
+let arrayItems = [];
+
+function Iniciodecompra(arrayItems) {
     let producto = prompt("Indique el producto que desea");
     let precio = parseInt(prompt("Ingrese el precio"));
 
-    const nuevaCompra = new Compra (producto, precio);
+    const nuevoItem = new Items (producto, precio);
 
-    arrayItems.push(nuevaCompra);
-    console.log("Producto agregado");
+    arrayItems.push(nuevoItem);
+    console.log("Producto agregado"+ producto+ precio);
 }
 
-   function ContinuarCompra() {
-    arrayItems.forEach((Compra) => {
+function ContinuarCompra() {
+    arrayItems.forEach((Items) => {
       alert( "Has agregado al carrito" + producto +" por $" + (precio * 1.21));
     });
-  }
-    
-  let cliente;
-  function nombreCliente(){
+}
+
+function NombreCliente(){
        cliente = prompt ("ingrese su nombre para continuar la compra");
-      
+
       while ( cliente === ""){
         cliente = prompt ("ingrese su nombre para continuar la compra");
        }
        return cliente;
-     }
+ }  
+  
+function CuponDescuento() {
+     const giftcard = precio.filter((el) => el.precio > 100);
+        for ( const giftcard of giftcard) {
+        alert("¡Felicitaciones!"+ giftcard.cliente + "por tu compra de" + giftcard.precio + " recibiste un cupon de descuento de $600 para tu próxima compra");
+       }
+}
 
- function FinalizarCompra() {
-  const giftcard = precio.filter((el) => el.precio > 100);
-  for ( const giftcard of giftcard) {
-    alert("¡Felicitaciones!"+ giftcard.cliente + "por tu compra de" + giftcard.precio + " recibiste un cupon de descuento de $600 para tu próxima compra");
-  }
+ let opciones = prompt(" Seleccione una opción: \n 1) Iniciar compra \n 2) Ver compra \n 3) Cliente \n 4) Finalizar compra \n 5) Salir");
+
+ while( opciones !== "5") {
+       if ( opciones === "1") {
+        Iniciodecompra(arrayItems);
+       }
+       if ( opciones ==="2") {
+        ContinuarCompra(arrayItems);
+       }
+       if ( opciones === "3") {
+        NombreCliente(arrayItems);
+       }
+       if ( opciones === "4") {
+        CuponDescuento(arrayItems); 
+       }
+       opciones = prompt(" Seleccione una opción: \n 1) Iniciar compra \n 2) Ver compra \n 3) Cliente \n 4) Finalizar compra \n 5) Salir");
  }
- 
-alert("¡Gracias  por comprar en BAZAR ROSARITO!");
+
+
+ alert("¡Gracias  por comprar en BAZAR ROSARITO!");
  
